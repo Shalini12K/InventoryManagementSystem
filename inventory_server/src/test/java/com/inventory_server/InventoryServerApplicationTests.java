3211de58525f763486583dd00148fa58b93fdf5e
Shalini12K <shalini.kavee@gmail.com>
2026-01-16 13:53:48 +0530

    Add files via upload

---
+++ inventory_server/src/test/java/com/inventory_server/InventoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
