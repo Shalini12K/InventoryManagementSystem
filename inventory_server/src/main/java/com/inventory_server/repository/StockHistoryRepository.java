b9da78317628016aecebbd3b9ec20834871d46a7
Mahen <100280545+mistdevs@users.noreply.github.com>
2026-01-16 14:03:42 +0530

    Delete inventory_server/src/main/java/com/inventory_server/repository directory

--- inventory_server/src/main/java/com/inventory_server/repository/StockHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inventory_server.repository;
-
-import com.inventory_server.model.StockHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StockHistoryRepository extends JpaRepository<StockHistory, Long> {
-    List<StockHistory> findAllByOrderByIdDesc();
-}
